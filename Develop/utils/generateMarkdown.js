b5f4dc83878e9643f7319470d5a7af49786d1c0c
Brad Williams <jb.wms89@gmail.com>
2020-06-26 14:49:23 -0400

    added backticks using unicode

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -16,26 +16,35 @@ function generateMarkdown(data) {
 * [Questions](#questions)
 
 ## License
+\u0060\u0060\u0060
 This project is being licensed under the ${data.license} license.
-
+\u0060\u0060\u0060
 ## Description
+\u0060\u0060\u0060md
 ${data.description}
-
+\u0060\u0060\u0060
 ## Installation
+\u0060\u0060\u0060
 ${data.installation}
-
+\u0060\u0060\u0060
 ## Usage
+\u0060\u0060\u0060
 ${data.usage}
-
+\u0060\u0060\u0060
 ## Contributing
+\u0060\u0060\u0060
 ${data.contributing}
-
+\u0060\u0060\u0060
 ## Tests
+\u0060\u0060\u0060
 ${data.tests}
-
+\u0060\u0060\u0060
 ## Questions
+\u0060\u0060\u0060
 If you have any questions, follow my GitHub profile here: https://github.com/${data.github}.
+
 You can also email me at ${data.email}.
+\u0060\u0060\u0060
 `;
 }
 
